d1d061bc8928eebfd3f6ad190e4578745079224a
Thorinwasher <ragnarlol96@gmail.com>
2024-06-14 19:57:24 +0200

    Update and optimise plugin build script

--- docker/maven/compile.sh
+++ docker/maven/compile.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-mkdir GitHub
-cd GitHub
-git clone "https://github.com/stargate-rewritten/Stargate-Bukkit"
-git clone "https://github.com/stargate-rewritten/Stargate-Mechanics"
-git clone "https://github.com/stargate-rewritten/Stargate-Mapper"
-git clone "https://github.com/stargate-rewritten/Stargate-Customizations"
-git clone "https://github.com/stargate-rewritten/Stargate-Interfaces"
-
 cd /GitHub/Stargate-Bukkit
+git pull origin
 mvn clean install -DskipTests
 cp /GitHub/Stargate-Bukkit/target/Stargate-*.jar /plugins
+
 cd /GitHub/Stargate-Mechanics
-mvn clean install -DskipTests
-cp /GitHub/Stargate-Mechanics/target/StargateMechanics-*.jar /plugins
+git pull origin
+bash gradlew clean install
+cp /GitHub/Stargate-Mechanics/build/libs/StargateMechanics-*.jar /plugins
+
 cd /GitHub/Stargate-Mapper
+git pull origin
 mvn clean install -DskipTests
 cp /GitHub/Stargate-Mapper/target/StargateMapper-*.jar /plugins
+
 cd /GitHub/Stargate-Customizations
+git pull origin
 mvn clean install -DskipTests
 cp /GitHub/Stargate-Customizations/target/StargateCustomizations-*.jar /plugins
+
 cd /GitHub/Stargate-Interfaces
+git pull origin
 mvn clean install -DskipTests
 cp /GitHub/Stargate-Interfaces/target/StargateInterfaces-*.jar /plugins
-cd ~
