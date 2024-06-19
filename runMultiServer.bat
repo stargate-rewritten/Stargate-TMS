56b22340d04c77f8aff2f0380094932d6c2c5b15
Thorinwasher <ragnarlol96@gmail.com>
2024-06-19 11:08:02 +0200

    Temporary fix for `Operation not permitted` issue

--- runMultiServer.bat
+++ runMultiServer.bat
@@ -5,8 +5,10 @@ mkdir .\data\servers\velocity
 
 robocopy ./plugins ./data/servers/server1/plugins
 robocopy ./plugins ./data/servers/server2/plugins
-copy velocity.toml .\data\servers\velocity\velocity.toml /a
-copy forwarding.secret .\data\servers\velocity\forwarding.secret /a
+copy configs\velocity.toml .\data\servers\velocity\velocity.toml /a
+copy configs\forwarding.secret .\data\servers\velocity\forwarding.secret /a
+copy configs\spigot.yml .\data\servers\server1\spigot.yml /a
+copy configs\spigot.yml .\data\servers\server2\spigot.yml /a
 
 podman compose -f docker-compose-multi-server.yml up
 PAUSE
